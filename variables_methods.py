4630f1af88e8a1acd0a782f379743aa8f939e1fb
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-02-25 15:26:16 +0300

    Tested variables methods.

---
+++ variables_methods.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+my_variable = 56
+any_variable_name = 10
+
+string_variable = "Hello!"
+single_quotes = 'Strings can also have single quotes'
+
+print(my_variable)
+print(string_variable)
