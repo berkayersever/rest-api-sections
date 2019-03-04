2be0b3812e1db15df62cac7a9d6a1412738859af
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-04 13:45:57 +0300

    Moved variables_methods.py to section2.

---
+++ section2/variables_methods.py
@@ -0,0 +1,26 @@
+a = 5
+b = 10
+my_variable = 56
+any_variable_name = 10
+
+string_variable = "Hello!"
+single_quotes = 'Strings can also have single quotes'
+
+# print(my_variable)
+# print(string_variable)
+
+
+# Alternative Print Method
+def my_print_method(string):
+    print(string)
+
+
+# Alternative Multiplication Method
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+
+# my_print_method(my_variable)
+# my_print_method(string_variable)
+# result = my_multiply_method(5, 3)
+print(my_multiply_method(5, 3))
