c89d02f26fcfdb3b68013afe63e114b52063a0b6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-05 21:34:25 +0300

    Created two dictionaries.

--- section4/code/security.py
+++ section4/code/security.py
@@ -5,3 +5,17 @@ users = [
         'password': 'super'
     }
 ]
+
+username_mapping = {'bob': {
+        'id': 1,
+        'username': 'bob',
+        'password': 'super'
+    }
+}
+
+user_id_mapping = {1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'super'
+    }
+}
