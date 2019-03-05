109bd6615e2f08422b5e7d068614a884ec95fc9c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-05 21:38:17 +0300

    Implemented authentication and identity methods.

--- section4/code/security.py
+++ section4/code/security.py
@@ -19,3 +19,14 @@ user_id_mapping = {1: {
         'password': 'super'
     }
 }
+
+
+def authentication(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return user_id_mapping.get(user_id, None)
