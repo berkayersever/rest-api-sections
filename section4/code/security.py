e4e6614582454390a6ef775e5f5b436e0b8894c3
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-05 21:46:48 +0300

    Replaced the hardcoded dictionary with User class object.

--- section4/code/security.py
+++ section4/code/security.py
@@ -4,12 +4,7 @@ users = [
     User(1, 'bob', 'super')
 ]
 
-username_mapping = {'bob': {
-        'id': 1,
-        'username': 'bob',
-        'password': 'super'
-    }
-}
+username_mapping = {u.username: u for u in users}
 
 user_id_mapping = {1: {
         'id': 1,
