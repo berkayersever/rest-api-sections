258d19c63b76547d9029e6dd2bbf2c96537bbd5c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-06 23:01:42 +0300

    Removed the users lists.

--- section5/code/security.py
+++ section5/code/security.py
@@ -1,13 +1,6 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'bob', 'super')
-]
-
-username_mapping = {u.username: u for u in users}
-user_id_mapping = {u.id: u for u in users}
-
 
 def authenticate(username, password):
     user = User.find_by_username(username)
@@ -17,4 +10,4 @@ def authenticate(username, password):
 
 def identity(payload):
     user_id = payload['identity']
-    return user_id_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
