54dc70c619263e92fc9671aca5009e91a25e8cf1
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-06 19:04:22 +0300

    Implemented find_by_username method.

--- section5/code/user.py
+++ section5/code/user.py
@@ -1,5 +1,26 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
