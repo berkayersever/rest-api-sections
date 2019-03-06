d9bc231b305f234d041f78ffaa7d07f652e348f8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-06 23:37:22 +0300

    Minor change in the database path.

--- section5/test.py
+++ section5/test.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('data.db')
+connection = sqlite3.connect('code/data.db')
 cursor = connection.cursor()
 create_table = "CREATE TABLE users (id int, username text, password text)"
 cursor.execute(create_table)
