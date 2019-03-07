418b113a1444628ba936189011dd485aa08b04f3
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-07 21:54:46 +0300

    Removed the items list.

--- section5/code/app.py
+++ section5/code/app.py
@@ -10,8 +10,6 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
-items = []
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
