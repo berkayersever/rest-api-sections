59bd40e2b700f8041306a09395e063933cbc4e40
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-07 21:59:32 +0300

    Removed unnecessary imported packages.

--- section5/code/app.py
+++ section5/code/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_restful import Api, reqparse
+from flask import Flask
+from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
 from user import UserRegister
