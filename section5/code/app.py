9f5fadb878163d244acfad6d0ba32f113701e771
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-07 21:56:16 +0300

    Imported Item and ItemList classes from item.py

--- section5/code/app.py
+++ section5/code/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'fake'  # Should be a Secret Key
