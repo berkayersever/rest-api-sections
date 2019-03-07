e86f32b6214abddfdc53ec578dc1e4d3d06f0b4e
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-07 17:25:21 +0300

    Implemented the encrypt_string method.

--- section5/code/user.py
+++ section5/code/user.py
@@ -48,6 +48,8 @@ class UserRegister(Resource):
     parser.add_argument('password', type=str, required=True, help="This field cannot be left blank!")
 
     def encrypt_string(self, hash_string):
+        sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
+        return sha_signature
 
     def post(self):
         data = UserRegister.parser.parse_args()
