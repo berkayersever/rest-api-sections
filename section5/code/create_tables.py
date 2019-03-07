82e96dbccc74d513f13baf1eb2d18964d80c1f73
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-07 22:13:29 +0300

    Minor additions to create items table.

--- section5/code/create_tables.py
+++ section5/code/create_tables.py
@@ -4,9 +4,15 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-insert_admin = "INSERT INTO users (id, username, password) VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918')"
 cursor.execute(create_table)
+
+insert_admin = "INSERT INTO users (id, username, password) VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918')"
 cursor.execute(insert_admin)
+
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table)
+
+cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
 
 connection.commit()
 connection.close()
