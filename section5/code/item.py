db6b30769569e3a7128c1658e26c828e607dd2ff
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-13 00:19:46 +0300

    Updated the put method for the database connection.

--- section5/code/item.py
+++ section5/code/item.py
@@ -58,12 +58,12 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            self.insert(updated_item)
         else:
-            item.update(data)
+            self.update(updated_item)
         return item
 
 
